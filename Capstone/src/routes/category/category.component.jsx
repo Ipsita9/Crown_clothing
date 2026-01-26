0b998337ad82b1caaaef31d13b54a2fa6c3f20c1
Ipsita Rauta <rautaipsita@gmail.com>
2026-01-26 23:14:29 +0530

    Update redox tool kit

--- Capstone/src/routes/category/category.component.jsx
+++ Capstone/src/routes/category/category.component.jsx
@@ -1,44 +1,4 @@
-// import { useState,useEffect, Fragment} from "react";
-// import { useParams } from "react-router-dom";
-// import { useSelector } from "react-redux";
-// import { selectCategoriesMap } from "../../store/categories/category.selector.js";
-// import ProductCard from "../../component/product-card/product-card.component";
 
-// import {CategoryContainer ,Title} from "./category.style.jsx";
-
-// const Category = () => {
-//   console.log("Category file loaded");
-
-//   const { category } = useParams();
-//   console.log("render/re-rendering category component")
-//   const categoriesMap=useSelector(selectCategoriesMap);
-//   const [products, setProducts] = useState([]);
-
-//   useEffect(() => {
-//     console.log('effect fired calling setproducts');
-//     setProducts(categoriesMap[category] || []);
-//   }, [category, categoriesMap]);
-
-//     if (!products.length) {
-//     return <h2>Loading products...</h2>;
-//   }
-
-
-//   return (
-//      <Fragment><Title>{category}</Title> 
-     
-//     <CategoryContainer>
-       
-//       {products &&
-//       products.map((product) => (
-//         <ProductCard key={product.id} product={product} />
-//       ))}
-//     </CategoryContainer>
-//     </Fragment>
-//   );
-// };
-
-// export default Category;
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Fragment } from "react";
